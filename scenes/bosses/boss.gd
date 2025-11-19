cce96faf8c03c680b52dc9e7037cf9c45c712862
Nicolas Graveret <neylick@gmail.com>
2025-11-19 10:20:50 +0100

    Boss projectile & attack anim

--- scenes/bosses/boss.gd
+++ scenes/bosses/boss.gd
@@ -1,20 +1,36 @@
 extends Mob
 
+@export var projectile : PackedScene
+var projectileBaseCooldown = 5.0
+var currentProjectileCooldown : float
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 	
 func attack() :
 	$BossAnims.play("attack")
+	isAttacking = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	super._process(delta)
-	if(!isBlocked):
-		$BossAnims.play("walk")
+	if(!isAttacking):
+		$BossAnims.play("idle" if isBlocked else "walk")
+	
+	currentProjectileCooldown -= delta
+	if(currentProjectileCooldown < 0):
+		currentProjectileCooldown = projectileBaseCooldown
+		var proj = projectile.instantiate()
+		proj.position = position
+		proj.position.x -= 20
+		proj.position.y -= 250
+		add_sibling(proj)
+		
 
 func _on_boss_anims_animation_finished() -> void:
 	$BossAnims.play("idle")
+	isAttacking = false
 
 
 func _on_block_area_area_entered(area: Area2D) -> void:
